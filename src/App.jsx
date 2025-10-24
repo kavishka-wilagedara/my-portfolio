becbdcfe6f40e103af875b78af68873c0fc5a8c1
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-10-24 18:07:02 +0530

    rename file

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from "./sections/About";
 import Project from "./sections/Project";
 import Contact from "./sections/Contact";
 import Footer from "./components/Footer";
-import LoadingAnimation from "./components/LoadingAnimatio";
+import LoadingAnimation from "./components/LoadingAnimation";
 
 function App() {
   const [loading, setLoading] = useState(true);
